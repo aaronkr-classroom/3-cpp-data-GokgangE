034266a50c3772ccf3425a129f1f58474d9e940c
Jaemin <GokgangE8109@gmail.com>
2025-03-21 16:28:03 +0900

    3주차

---
+++ Week3/square.cpp
@@ -0,0 +1,15 @@
+//
+#include<iostream>
+#include<string>
+
+using std::cout;		using std::cin;
+using std::endl;		using std::string;
+
+
+int main() {
+	stinrg square_len = 5;
+	for (int r = 0; r < square_len; r++) {
+		string row(square_len, "*");
+
+	}
+}
